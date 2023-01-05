3fdba9a559fb590f524570e609c183d26d4f3144
palewire <b@palewi.re>
2023-01-05 14:03:30 -0800

    Clean it up

--- reutersjobs/app.py
+++ reutersjobs/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 
 from . import utils
@@ -7,15 +6,17 @@ from . import utils
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def job_list():
-    return render_template('job_list.html', obj_list=utils.get_latest_list())
+    """List all of the jobs in the latest clean file."""
+    return render_template("job_list.html", obj_list=utils.get_latest_list())
 
 
 @app.route("/jobs/")
 def job_detail():
+    """Serve a detail page that we'll save as an image."""
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return f"Hello, {escape(name)}!"
 
 
 if __name__ == "__main__":
