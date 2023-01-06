bdf416a2915eba514148e44e4a473b7ae06c9c00
palewire <b@palewi.re>
2023-01-05 16:01:13 -0800

    Craft a little system for cleaning it up

--- reutersjobs/app.py
+++ reutersjobs/app.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+from dateutil.parser import parse as dateparse
 from flask import Flask, render_template, request
 from markdown import markdown
 
@@ -18,8 +20,36 @@ def job_detail():
     id_ = request.args.get("id")
     obj_list = utils.get_latest_list()
     obj = next(o for o in obj_list if o["id"] == id_)
-    obj["html"] = markdown(obj["description"])
+    obj["html"] = _prep_html(obj["description"])
+    obj["open_date"] = dateparse(obj["open_date"])
     return render_template("job_detail.html", obj=obj)
+
+
+def _prep_html(s):
+    html = markdown(s)
+    soup = BeautifulSoup(html, "html5lib")
+    for each in soup.findChildren():
+        if _cut(each.text):
+            each.clear()
+    return str(soup)
+
+
+def _cut(s):
+    s = s.lower().strip()
+    if s in ["accessibility"]:
+        return True
+    starts_list = [
+        "as a global business, we rely on diversity of culture",
+        "We also make reasonable accommodations",
+        "Protect yourself from fraudulent job postings",
+        "More information about Thomson Reuters can be found",
+        "We are powered by the talents of",
+        "Do you want to be part of a team helping re-invent the way knowledge professionals work?",
+    ]
+    for t in starts_list:
+        if s.startswith(t.lower()):
+            return True
+    return False
 
 
 if __name__ == "__main__":
